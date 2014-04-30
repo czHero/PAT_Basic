180aecc32f0bea7f1ad8b53d8fe5fd15ee0b495a
czHero <chizhang@live.cn>
2014-04-30 10:29:17 +0800

    1019已通过
    发现原来是list.clear() 导致一直抛出异常。很想不通
    可能是python版本不一致吧。

--- 1019.py
+++ 1019.py
@@ -1,20 +1,23 @@
 import sys
 strA = input()
-strlist = []
+
 time = 1
 strA="%04d"%(int(strA.replace(" ","")))
 
-while (strA!='6174' and strA!='0000') or time!=0:
+while (strA!="6174" and strA!="0000") or time!=0:
 	if time!=0:
 		time = time-1
-	strlist.clear()
+	strlist = []		
 	for c in strA:
 		strlist.append(c)
 	strlist.sort()
 	strLow=''
+
 	for str in strlist:
 		strLow+=str
-	strHigh= strLow[::-1]
+	
+	strHigh= strLow[::-1]	
 	numAns=int(strHigh)-int(strLow)
 	strA="%04d"%(numAns)
 	print (strHigh,'-',strLow,'=',strA)
+	
